5c1a3e0ff41641323979f8ba266afc930e16fe52
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-04-23 23:26:39 -0600

    adds 404 test

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_sees_error_status_code
+    visit '/comedians'
+
+    assert page.has_content?("Error: this page does not exist.")
+    assert_equal 404, page.status_code
+  end
 end
